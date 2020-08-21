456842523a59c58d9eec1b0892e7789928ed5c87
ryan-bello <ryanbello48@gmail.com>
2020-08-21 19:03:30 -0400

    Rebase completed

--- goals_checklist.c
+++ goals_checklist.c
@@ -2,27 +2,59 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 void check_errors(int error_status);
+void print_date(struct tm * date, int display_time);
 int print_current_goals();
 int print_past_goals();
-int print_all_goals();
+int print_all_goals(); // is this function really necessary?
+
+const int MAX_CHARS = 99;
+const char * DATE_NO_TIME = "%x";
+const char * DATE_W_TIME = "%x - %I:%M%p";
+
+struct goal{
+
+};
 
 int main()
 {
-  check_errors(print_past_goals());
+  time_t rawtime;
+  struct tm *timeinfo;
+
+  time(&rawtime);
+  timeinfo = localtime (&rawtime);
+
+  printf("Current local time and date: %s", asctime(timeinfo));
+
+  print_date(timeinfo, 0);
+
+  print_date(timeinfo, 1);
 
 
-  
 }
 
 void check_errors(int error_status)
 {
   switch (error_status) {
     case 0:
-      return; //shouldn't need to break because we are returning
+      return;
     default:
-      print("Error encountered");
+      printf("Error encountered");
       return;
   }
+}
+
+int print_current_goals(){
+  return 0;
+}
+
+void print_date(struct tm * date, int display_time){
+  char * format_type = display_time == 0 ? DATE_NO_TIME : DATE_W_TIME;
+  char date_string [MAX_CHARS];
+
+  strftime(date_string, MAX_CHARS, format_type, date); //can i drop the size_t?
+
+  printf("%s\n", date_string);
 }
